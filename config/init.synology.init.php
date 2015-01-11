edb12b5cb276c1c0c4cd58d06c82d47feb6d598d
Vincent Pelisset <vinpel@hotmail.com>
2015-01-11 14:26:34 +0000

    synology config file for the package

---
+++ config/init.synology.init.php
@@ -0,0 +1,9 @@
+<?php
+ return array (
+  'publicURI' => 'https://@wizard_calibre_hostname@:@wizard_gasp_port@',
+  'host' => '@wizard_gasp_dbhost@',
+  'database' => '@wizard_gasp_dbname@',
+  'username' => '@wizard_gasp_dblogin@',
+  'password' => '@wizard_gasp_dbpassword@',
+)
+?>
