7f74a6d39eee1d27b43717d411d24439274e451e
Vincent Pelisset <vinpel@hotmail.com>
2015-01-12 11:56:19 +0000

    delete 2 file for synology, they are in the spk config now

--- config/init.synology.init.php
+++
@@ -1,9 +0,0 @@
-<?php
- return array (
-  'publicURI' => 'https://@wizard_calibre_hostname@:@wizard_gasp_port@',
-  'host' => '@wizard_gasp_dbhost@',
-  'database' => '@wizard_gasp_dbname@',
-  'username' => '@wizard_gasp_dblogin@',
-  'password' => '@wizard_gasp_dbpassword@',
-)
-?>
